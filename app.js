94dee9274812cdedb173fcba9ee0337e7c9becb4
Amesh Alex Kuruvilla <finixbenny@Ameshs-Air.fios-router.home>
2020-07-31 17:12:23 -0400

    created a clicker on field

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ $(() => {
   const mineBoard =  () => {
   
     const $field = $('#game');
-    for (i=0; i<10 ; i++) {
+    for (i=0; i<180 ; i++) {
       let $box = $('<div>')
       $box.addClass('box')
       $box.attr('id', i)
-      $field.append($box.)
+      $field.append($box)
+      
 
     }
 
   }
+  const sweepMine= (event) => {
 
+    console.log(event.currentTarget.id);
+  }
 
   mineBoard();
+  //creating on clicker on field
+  $('.box').on('click', sweepMine);
+
 })
