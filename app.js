c92263e8bf86d8f56152a75d3136eb6f443b03dd
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-07-31 20:38:15 -0400

    created somewhat functioning logic

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ $(() => {
   const mineBoard =  () => {
   
     const $field = $('#game');
-    for (i=0; i<180 ; i++) {
+    for (i=0; i<100 ; i++) {
       let $box = $('<div>')
       $box.addClass('box')
       $box.attr('id', i)
-      $box.text(i)
-
       $field.append($box)
-
-
+      $box.text(i)
     }
-
   }
   
   const arr = [];
@@ -22,22 +18,63 @@ $(() => {
   //create function to make mines.
   const mines = (num) => {
     for (let i=0; i<=num; i++) {
-      arr.push(Math.floor(Math.random() * 180))
+      arr.push(Math.floor(Math.random() * 99))
     }
   }
   mines(10);
   console.log(arr);
 
+  const selectedArr = [];
+
   //Clicking on field to detect mines.
   const sweepMine= (event) => {
-    if (arr.includes(parseInt(event.currentTarget.id,10))) {
+    const numb = event.currentTarget;
+    if (arr.includes(parseInt(numb.id,10))) {
       console.log('dead')
-      console.log(event.currentTarget);
-      $(event.currentTarget).css('background-color', 'red')
+      console.log(numb);
+      $(numb).css('background-color', 'red')
     } else {
       console.log('alive');
-      console.log(event.currentTarget.id);
-      $(event.currentTarget).css('background-color', 'green')
+      console.log(numb.id);
+      $(numb).css('background-color', 'green')
+
+      
+        if(arr.includes(parseInt(numb.id,10)+1)) {
+        selectedArr.push(parseInt(numb.id)+1)
+        console.log(selectedArr, true);
+        console.log(selectedArr.length)
+      }
+      if(arr.includes(parseInt(numb.id,10)-1)) {
+        selectedArr.push(parseInt(numb.id)-1)
+        console.log(selectedArr, true);
+      }
+      if(arr.includes(parseInt(numb.id,10)+10)) {
+        selectedArr.push(parseInt(numb.id)+10)
+        console.log(selectedArr, true);
+      }
+      if(arr.includes(parseInt(numb.id,10)-10)) {
+        selectedArr.push(parseInt(numb.id)-10)
+        console.log(selectedArr, true);
+      }
+      if(arr.includes(parseInt(numb.id,10)+11)) {
+        selectedArr.push(parseInt(numb.id)+11)
+        console.log(selectedArr, true);
+      }
+      if(arr.includes(parseInt(numb.id,10)-11)) {
+        selectedArr.push(parseInt(numb.id)-11)
+        console.log(selectedArr, true);
+      }
+
+      if(arr.includes(parseInt(numb.id,10)+9)) {
+        selectedArr.push(parseInt(numb.id)+9)
+        console.log(selectedArr, true);
+      }
+      if(arr.includes(parseInt(numb.id,10)-9)) {
+        selectedArr.push(parseInt(numb.id)-9)
+        console.log(selectedArr, true);
+      }
+      
+
     }
   }
 
